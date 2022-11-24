ce27a33936916a51cfb73c649f2175380e192658
ivanFlor3s <ifloresmanc96@gmail.com>
2022-11-23 22:38:07 -0300

    Navbar + espacio drawer (bar side mui)

--- src/journal/layout/JournalLayout.tsx
+++ src/journal/layout/JournalLayout.tsx
@@ -1,13 +1,15 @@
 import { Box } from "@mui/material";
+import { Navbar } from '../components/Navbar';
 
 interface Props {
   children: React.ReactNode;
 }
-
+const drawerWidth = 240
 export const JournalLayout = ({ children }: Props) => {
   return (
     <Box sx={{ display: "flex" }}>
       {/* NAVBAR */}
+      <Navbar drawerWidth={drawerWidth}></Navbar>
       {/* SIDEBAR */}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/*Toolbar */}
