6695c696c0a29a10f4edb663d2bd75aab61234a1
ivanFlor3s <ifloresmanc96@gmail.com>
2022-11-23 23:24:43 -0300

    Journal app sidebar

---
+++ src/journal/components/Sidebar.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Drawer,
+  ListItem,
+  List,
+  Toolbar,
+  Typography,
+  ListItemText,
+  Grid,
+  ListItemButton,
+  ListItemIcon,
+  Divider
+} from "@mui/material";
+import { TurnedInNot } from "@mui/icons-material";
+
+interface Props {
+  drawerWidth: number;
+}
+export const Sidebar = ({ drawerWidth = 240 }: Props) => {
+  return (
+    //TODO revisar estilo sidebar
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth}, flexShrink: { sm: 0 }  }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Ivan Flores
+          </Typography>
+        </Toolbar>
+
+          <Divider></Divider>
+          <List>
+            {["Enero", "Marzo", "Feb", "Abril"].map((text) => (
+              <ListItem key="text" disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <TurnedInNot></TurnedInNot>
+                  </ListItemIcon>
+                  <Grid container>
+                    <ListItemText primary={text}></ListItemText>
+                    <ListItemText
+                      secondary={"lorem lorem lorem"}
+                    ></ListItemText>
+                  </Grid>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+      </Drawer>
+    </Box>
+  );
+};
--- src/journal/layout/JournalLayout.tsx
+++ src/journal/layout/JournalLayout.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material";
-import { Navbar } from '../components/Navbar';
+import { Box, Toolbar } from "@mui/material";
+import { Navbar, Sidebar } from "../components";
+
 
 interface Props {
   children: React.ReactNode;
@@ -10,9 +11,12 @@ export const JournalLayout = ({ children }: Props) => {
     <Box sx={{ display: "flex" }}>
       {/* NAVBAR */}
       <Navbar drawerWidth={drawerWidth}></Navbar>
+      
       {/* SIDEBAR */}
+      <Sidebar drawerWidth={drawerWidth}></Sidebar>
+
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/*Toolbar */}
+        <Toolbar/>
         {children}
       </Box>
     </Box>
